88247e6c52f10fd44e4152119ef4eb22c52ff7cc
biskwikman <djosephhenri@gmail.com>
2024-06-17 00:35:08 +0900

    first

--- src/bin2nc.jl
+++ src/bin2nc.jl
@@ -1,5 +1,22 @@
 module bin2nc
+using NetCDF
 
-# Write your package code here.
+# function main()
+#     directory = "/media/storage/Work/30km/modis_data"
+#     get_bin_files(directory)
+# end
+
+function get_bin_files(directory::String)
+    !Base.Filesystem.isdir(directory) ? 
+        throw(DomainError(directory, "argument must be a directory")) : 
+        nothing
+
+    dir_files = Base.Filesystem.readdir(directory)
+    println("ok")
+    
+end
+
+# main()
 
 end
+
